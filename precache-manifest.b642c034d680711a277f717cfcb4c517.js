25fb5e70a2aff4d540a7d26f769c402760340b8d
oznerek <oznerek@gmail.com>
2019-08-07 11:43:19 +0200

    Updates

---
+++ precache-manifest.b642c034d680711a277f717cfcb4c517.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "649d563a81fcc9511ad32db01d5a2642",
+    "url": "/Polluted-cities/index.html"
+  },
+  {
+    "revision": "c112066063056ca421b1",
+    "url": "/Polluted-cities/static/js/2.cb1a62d1.chunk.js"
+  },
+  {
+    "revision": "00f5da98ed7bac73b716",
+    "url": "/Polluted-cities/static/js/main.829a6efc.chunk.js"
+  },
+  {
+    "revision": "826fa3e0bc19eb5f7841",
+    "url": "/Polluted-cities/static/js/runtime~main.4852b0a6.js"
+  }
+]);
